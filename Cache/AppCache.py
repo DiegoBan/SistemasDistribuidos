7fb91e10eafd3bf886e1fcafee675de324f892c1
DiegoBan <bandadiego452@gmail.com>
2025-04-30 04:23:02 -0400

    esta vivoooo🗣️

--- Cache/AppCache.py
+++ Cache/AppCache.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
 import utilidadesRedis
 import utilidadesMongodb
 
@@ -8,11 +9,11 @@ async def lifespan(app: FastAPI):
     utilidadesRedis.startup("allkeys-lru", "100mb") #   Politica y tamaño
     yield
     print("Adios...")
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/generalKenobi")  #   Health, si está arriba el contenedor
 def generalKenobi():
-    return {"Obi Wan Kenobi": "Hello there!"}
+    return {"Obi-Wan Kenobi": "Hello there!"}
 
 @app.get("/redis/status")   #   Si está arriba redis
 def redisStatus():
@@ -22,18 +23,19 @@ def redisStatus():
 def mongodbStatus():
     return utilidadesMongodb.mongoPing()
 
-@app.get("/cache/{alertType}/{UUID}")
+@app.get("/cache/{alertType}/{UUID}")   #   Llamar a este endpoint como consulta a sistema
 def cache(alertType: str, UUID: str):
-    if utilidadesRedis.isInCache(UUID):
+    if utilidadesRedis.isInCache(UUID): #   Si está en cache retorna true (hit)
         return True
-    else:
+    else:   #   Si no está en caché, lo agrega y retorna false (miss)
         value = utilidadesMongodb.getAlerta(alertType, UUID)
         utilidadesRedis.addKeyValue(UUID, value)
+        return False
 
-@app.get("cache/changepolicy")  #   Intercambia politica de remoción entre lru y lfu
+@app.get("/cache/changepolicy")  #   Intercambia politica de remoción entre lru y lfu
 def changePolicy():
     return utilidadesRedis.changePolicy()
 
-@app.get("/cache/changeSize/{size}")    #   size debe estar en mb
+@app.get("/cache/changesize/{size}")    #   size debe estar en mb
 def changeSize(size: int):
-    return utilidadesRedis.changeSize("{size}mb")
+    return utilidadesRedis.changeSize(f"{size}mb")
